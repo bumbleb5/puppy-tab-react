86aa0c3b985a09c56ab1f74e34781efa8ec55492
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-04-13 12:50:09 -0500

    saving input in local storage

--- src/components/addPet/addPetFormComponents/addPetForm.js
+++ src/components/addPet/addPetFormComponents/addPetForm.js
@@ -8,6 +8,14 @@ import AddPetMedsInput from './addPetMedsInput';
 import AddPetBreedInput from './addPetBreedInput';
 import AddPetSubmitButton from './addPetSubmitButton';
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
+  
+
 class AddPetForm extends React.Component {
     constructor(props) {
         super(props);
@@ -18,9 +26,11 @@ class AddPetForm extends React.Component {
             petSex: '',
             vetClinicName: '',
             petMeds: '',
-            petBreed: ''
+            petBreed: '',
+            petID: ''
         };
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleInputChange(e) {
@@ -30,11 +40,21 @@ class AddPetForm extends React.Component {
         console.log(this.state);
     }
 
+    handleSubmit(event) {
+        alert('A profile was submitted for ' + this.state.addPetName);
+        event.preventDefault();
+        const petID = uuidv4();
+        this.setState({ petID: petID });
+        const stringifiedPet = JSON.stringify(this.state);
+        console.log(stringifiedPet);
+        localStorage.setItem(this.state.petID, stringifiedPet);
+    }
+
     render() {
         return (
             <div>
                 <h1 id="addPetTitle">Add Pet</h1>
-                <form id="addPetForm" name="addPetForm">
+                <form id="addPetForm" name="addPetForm" onSubmit={ this.handleSubmit }>
                     <AddPetNameInput handleChange={ this.handleInputChange } value={ this.state.addPetName }/>
                     <AddPetSpeciesInput handleChange={ this.handleInputChange } value={ this.state.petSpecies }/>
                     <AddPetBirthdateInput handleChange={ this.handleInputChange } value={ this.state.petBirthDate }/>
--- src/components/addPet/addPetFormComponents/addPetSubmitButton.js
+++ src/components/addPet/addPetFormComponents/addPetSubmitButton.js
@@ -4,6 +4,7 @@ class AddPetSubmitButton extends React.Component {
     constructor() {
         super();
     }
+
     render() {
         return (
             <button type="submit" form="addPetForm" value="Add Pet">Add Pet</button>
