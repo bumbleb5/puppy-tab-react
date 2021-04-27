cddd49e06a4f942f856c8b527f34971e2bb833e9
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-04-27 11:26:18 -0500

    styling species input

--- src/components/addPet/addPetFormComponents/addPetSpeciesInput.js
+++ src/components/addPet/addPetFormComponents/addPetSpeciesInput.js
@@ -7,13 +7,19 @@ class AddPetSpeciesInput extends React.Component {
     render() {
         return (
             <div className="formField">
-                <label>Species</label><br/>
+                <label for="species">Species</label>
+                <select id="species" name="species" className="speciesDropDown">
+                    <option value="dog">Dog</option>
+                    <option value="cat">Cat</option>
+                    <option value="other">Other</option>
+                </select>
+                {/* <label>Species</label><br/>
                 <input type="radio" id="petSpeciesDog" name="petSpecies" value="dog" onChange={ this.props.handleChange }/>
                 <label htmlFor="petSpeciesDog" className="radioOptionLabel">Dog</label>
                 <input type="radio" id="petSpeciesCat" name="petSpecies" value="cat" onChange={ this.props.handleChange }/>
                 <label htmlFor="petSpeciesCat" className="radioOptionLabel">Cat</label>
                 <input type="radio" id="petSpeciesOther" name="petSpecies" value="other" onChange={ this.props.handleChange }/>
-                <label htmlFor="petSpeciesOther" className="radioOptionLabel">Other</label>
+                <label htmlFor="petSpeciesOther" className="radioOptionLabel">Other</label> */}
             </div>
         );
     }
